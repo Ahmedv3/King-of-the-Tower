82b2d5fad2c669cd5a4bbcaee09ad4a3acb970a4
Jakub Czarnecki <ahmed122.90@o2.pl>
2018-03-18 23:37:14 +0100

    Add files via upload

---
+++ Hero.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jummpHeight;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    private bool grounded;
+
+    void FixedUpdate(){
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    void Jump(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, jummpHeight);
+    }
+
+
+    // Use this for initializatio
+    void Start () {
+
+        
+		
+	}
+
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+            Jump();
+            
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+    }
+}
