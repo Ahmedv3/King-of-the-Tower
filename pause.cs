3716cf0b0cbe82725557126c9fec0b18cba71bbe
Jakub Czarnecki <ahmed122.90@o2.pl>
2018-05-23 20:25:02 +0200

    Add files via upload

---
+++ pause.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pause : MonoBehaviour {
+
+    public GameObject pauseUI;
+    private bool paused = true;
+
+
+    void Start(){
+        pauseUI.SetActive(true);
+    }
+
+    void Update(){
+
+        if (Input.GetButtonDown("Pause")){
+            paused = !paused;
+        }
+
+        if (paused){
+            pauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (!paused){
+            pauseUI.SetActive(false);
+            Time.timeScale = 1;
+
+        }
+
+
+    }
+
+    public void resume()
+    {
+        paused = false;
+    }
+
+    public void restart()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+}
