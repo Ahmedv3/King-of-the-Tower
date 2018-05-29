a2db1ceb9b4716d7da9366431d24056609aabd70
Jakub Czarnecki <ahmed122.90@o2.pl>
2018-05-29 12:39:37 +0200

    Add files via upload

---
+++ endScreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endScreen : MonoBehaviour {
+
+    public GameObject Hero;
+    public GameObject end;
+    private bool paused = false;
+
+
+    void Start()
+    {
+        end.SetActive(false);
+    }
+
+
+    void Update()
+    {
+
+        if (Hero.transform.position.y >= 47.0f)
+        {
+            paused = true;
+        }
+
+        if (paused)
+        {
+            end.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (!paused)
+        {
+            end.SetActive(false);
+            Time.timeScale = 1;
+
+        }
+
+
+
+    }
+}
